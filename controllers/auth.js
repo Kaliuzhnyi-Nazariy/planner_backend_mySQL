7e18704e6c04672cc998c92695388d6cc87269af
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-10-07 19:58:09 +0300

    add login/register logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,8 @@
 const db = require("../db.config");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const { SECRET_KEY } = process.env;
 
 const userProf = ({ insertId }) => {
   const q_ret_profile = "select username, email from users where id = ?";
@@ -11,34 +15,68 @@ const userProf = ({ insertId }) => {
   });
 };
 
-const register = (req, res) => {
-  const q =
-    "insert into users (username, email, password, token) values (?, ?, ?, ?)";
+const register = async (req, res) => {
+  const q = "insert into users (username, email, password) values (?, ?, ?)";
 
   const { username, email, password } = req.body;
 
-  db.query(q, [username, email, password, "token"], async (err, data) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  db.query(q, [username, email, hashedPassword], async (err, data) => {
     if (err) return res.status(409).json(err);
-    const newUser = await userProf(data);
-    return res.json(newUser);
+
+    const payload = {
+      id: data.insertId,
+    };
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+    const q_insert_token = "update users set token = ? where id = ?";
+
+    db.query(q_insert_token, [token, data.insertId], (err, data) => {
+      if (err) return res.status(409).json(err);
+      return res.json({
+        username,
+        email,
+        token,
+      });
+    });
   });
 };
 
-const login = (req, res) => {
-  const q = "SELECT username, id from users where email = ? and password = ?";
+const login = async (req, res) => {
+  const q = "SELECT * from users where email = ?";
+  const q_new_token = "update users set token = ? where id = ?";
 
   const { email, password } = req.body;
 
-  db.query(q, [email, password], (err, data) => {
-    if (err) return res.status(409).json(err);
-    return res.json(data);
+  db.query(q, email, async (err, data) => {
+    if (err) return res.status(404).json(err);
+    const comparedPassword = await bcrypt.compare(password, data[0].password);
+    if (!comparedPassword) return new Error();
+
+    const payload = {
+      id: data[0].id,
+    };
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+    db.query(q_new_token, [token, data[0].id], (err, data) => {
+      if (err) return res.status(409).json(err);
+    });
+
+    const { id, username, email } = data[0];
+
+    return res.json({ id, username, email, token });
   });
 };
 
 const logout = (req, res) => {
   const q = "update users set token = '' where id = ?";
 
-  db.query(q, 1, (err, data) => {
+  // id will be received from middleware authenticate
+
+  db.query(q, 49, (err, data) => {
     if (err) return res.status(409).json(err);
     return res.json(data);
   });
