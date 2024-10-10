84ab48d56caceb5a34df7abe8d0e78cfeef65085
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-10-10 19:39:42 +0300

    for deploy

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 require("dotenv").config();
 const app = require("./app");
 
-app.listen(3018, () => {});
+app.listen(3018, (err) => {
+  if (err) {
+    console.error("Error starting server:", err);
+  } else {
+    console.log("Server running on port 3018");
+  }
+});
